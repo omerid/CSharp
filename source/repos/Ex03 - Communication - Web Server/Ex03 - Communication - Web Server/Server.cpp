138e79e938209439018530912e0146aaa6cc3e2d
omerid <71331571+omerid@users.noreply.github.com>
2021-09-04 15:48:40 +0300

    Last vers to submit

--- source/repos/Ex03 - Communication - Web Server/Ex03 - Communication - Web Server/Server.cpp
+++ source/repos/Ex03 - Communication - Web Server/Ex03 - Communication - Web Server/Server.cpp
@@ -19,10 +19,10 @@ using namespace std;
 
 struct SocketState
 {
-	SOCKET id;			// Socket handle
-	int	recv;			// Receiving?
-	int	send;			// Sending?
-	int sendSubType;	// Sending sub-type
+	SOCKET id;			
+	int	recv;		
+	int	send;			
+	int sendSubType;
 	char buffer[1024];
 	int len;
 };
@@ -74,98 +74,59 @@ struct timeval timeout;
 
 void main()
 {
-	// Initialize Winsock (Windows Sockets).
-	// Create a WSADATA object called wsaData.
-	// The WSADATA structure contains information about the Windows 
-	// Sockets implementation.
+
 	WSAData wsaData;
 
-	// Call WSAStartup and return its value as an integer and check for errors.
-	// The WSAStartup function initiates the use of WS2_32.DLL by a process.
-	// First parameter is the version number 2.2.
-	// The WSACleanup function destructs the use of WS2_32.DLL by a process.
+
 	if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
 	{
-		cout << "Time Server: Error at WSAStartup()\n";
+		cout << "[Server] Error at WSAStartup()\n";
 		return;
 	}
 
-	// Server side:
-	// Create and bind a socket to an internet address.
-	// Listen through the socket for incoming connections.
-	// After initialization, a SOCKET object is ready to be instantiated.
-	// Create a SOCKET object called listenSocket. 
-	// For this application:	use the Internet address family (AF_INET), 
-	//							streaming sockets (SOCK_STREAM), 
-	//							and the TCP/IP protocol (IPPROTO_TCP).
+
 	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	// Check for errors to ensure that the socket is a valid socket.
-	// Error detection is a key part of successful networking code. 
-	// If the socket call fails, it returns INVALID_SOCKET. 
-	// The if statement in the previous code is used to catch any errors that
-	// may have occurred while creating the socket. WSAGetLastError returns an 
-	// error number associated with the last error that occurred.
+
 	if (INVALID_SOCKET == listenSocket)
 	{
-		cout << "Time Server: Error at socket(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at socket(): " << WSAGetLastError() << endl;
 		WSACleanup();
 		return;
 	}
 
-	// For a server to communicate on a network, it must bind the socket to 
-	// a network address.
-	// Need to assemble the required data for connection in sockaddr structure.
-	// Create a sockaddr_in object called serverService. 
+
 	sockaddr_in serverService;
-	// Address family (must be AF_INET - Internet address family).
+
 	serverService.sin_family = AF_INET;
-	// IP address. The sin_addr is a union (s_addr is a unsigned long 
-	// (4 bytes) data type).
-	// inet_addr (Iternet address) is used to convert a string (char *) 
-	// into unsigned long.
-	// The IP address is INADDR_ANY to accept connections on all interfaces.
+
 	serverService.sin_addr.s_addr = INADDR_ANY;
-	// IP Port. The htons (host to network - short) function converts an
-	// unsigned short from host to TCP/IP network byte order 
-	// (which is big-endian).
+
 	serverService.sin_port = htons(TIME_PORT);
 
-	// Bind the socket for client's requests.
 
-	// The bind function establishes a connection to a specified socket.
-	// The function uses the socket handler, the sockaddr structure (which
-	// defines properties of the desired connection) and the length of the
-	// sockaddr structure (in bytes).
 	if (SOCKET_ERROR == bind(listenSocket, (SOCKADDR *)&serverService, sizeof(serverService)))
 	{
-		cout << "Time Server: Error at bind(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at bind(): " << WSAGetLastError() << endl;
 		closesocket(listenSocket);
 		WSACleanup();
 		return;
 	}
 
-	// Listen on the Socket for incoming connections.
-	// This socket accepts only one connection (no pending connections 
-	// from other clients). This sets the backlog parameter.
+
 	if (SOCKET_ERROR == listen(listenSocket, 5))
 	{
-		cout << "Time Server: Error at listen(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at listen(): " << WSAGetLastError() << endl;
 		closesocket(listenSocket);
 		WSACleanup();
 		return;
 	}
 	addSocket(listenSocket, LISTEN);
 
-	// Accept connections and handles them one by one.
+
 	while (true)
 	{
-		// The select function determines the status of one or more sockets,
-		// waiting if necessary, to perform asynchronous I/O. Use fd_sets for
-		// sets of handles for reading, writing and exceptions. select gets "timeout" for waiting
-		// and still performing other operations (Use NULL for blocking). Finally,
-		// select returns the number of descriptors which are ready for use (use FD_ISSET
-		// macro to check which descriptor in each set is ready to be used).
+
 		fd_set waitRecv;
 		FD_ZERO(&waitRecv);
 		for (int i = 0; i < MAX_SOCKETS; i++)
@@ -182,15 +143,12 @@ void main()
 				FD_SET(sockets[i].id, &waitSend);
 		}
 
-		// Wait for interesting event.
-		// Note: First argument is ignored. The fourth is for exceptions.
-		// And as written above the last is a timeout, hence we are blocked if nothing happens.
 
 		int nfd;
 		nfd = select(0, &waitRecv, &waitSend, NULL, NULL);
 		if (nfd == SOCKET_ERROR)
 		{
-			cout << "Time Server: Error at select(): " << WSAGetLastError() << endl;
+			cout << "[Server] Error at select(): " << WSAGetLastError() << endl;
 			WSACleanup();
 			return;
 		}
@@ -229,7 +187,7 @@ void main()
 	}
 
 	// Closing connections and Winsock.
-	cout << "Time Server: Closing Connection.\n";
+	cout << "[Server] Closing Connection.\n";
 	closesocket(listenSocket);
 	WSACleanup();
 }
@@ -239,7 +197,7 @@ bool addSocket(SOCKET id, int what)
 	unsigned long flag = 1;
 	if (ioctlsocket(id, FIONBIO, &flag) != 0)
 	{
-		cout << "Time Server: Error at ioctlsocket(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at ioctlsocket(): " << WSAGetLastError() << endl;
 	}
 	for (int i = 0; i < MAX_SOCKETS; i++)
 	{
@@ -252,7 +210,7 @@ bool addSocket(SOCKET id, int what)
 			socketsCount++;
 			if (setsockopt(id, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) == SOCKET_ERROR)
 			{
-				cout << "Time Server: Error at setsockopt: " << WSAGetLastError() << endl;
+				cout << "[Server] Error at setsockopt: " << WSAGetLastError() << endl;
 				return false;
 			}
 			return true;
@@ -271,24 +229,21 @@ void removeSocket(int index)
 void acceptConnection(int index)
 {
 	SOCKET id = sockets[index].id;
-	struct sockaddr_in from;		// Address of sending partner
+	struct sockaddr_in from;		
 	int fromLen = sizeof(from);
 
 	SOCKET msgSocket = accept(id, (struct sockaddr *)&from, &fromLen);
 	if (INVALID_SOCKET == msgSocket)
 	{
-		cout << "Time Server: Error at accept(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at accept(): " << WSAGetLastError() << endl;
 		return;
 	}
-	cout << "Time Server: Client " << inet_ntoa(from.sin_addr) << ":" << ntohs(from.sin_port) << " is connected." << endl;
+	cout << "[Server] Client " << inet_ntoa(from.sin_addr) << ":" << ntohs(from.sin_port) << " is connected." << endl;
 
-	//
-	// Set the socket to be in non-blocking mode.
-	//
 	unsigned long flag = 1;
 	if (ioctlsocket(msgSocket, FIONBIO, &flag) != 0)
 	{
-		cout << "Time Server: Error at ioctlsocket(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at ioctlsocket(): " << WSAGetLastError() << endl;
 	}
 
 	if (addSocket(msgSocket, RECEIVE) == false)
@@ -310,11 +265,11 @@ void receiveMessage(int index)
 	{
 		if (WSAGetLastError() == WSAETIMEDOUT)
 		{
-			cout << "Time Server: Error at recv(): Too long to receive, time out triggered." << endl;
+			cout << "[Server] Error at recv(): Too long to receive, time out triggered." << endl;
 		}
 		else
 		{
-			cout << "Time Server: Error at recv(): " << WSAGetLastError() << endl;
+			cout << "[Server] Error at recv(): " << WSAGetLastError() << endl;
 		}
 		closesocket(msgSocket);
 		removeSocket(index);
@@ -329,7 +284,7 @@ void receiveMessage(int index)
 	else
 	{
 		sockets[index].buffer[len + bytesRecv] = '\0'; //add the null-terminating to make it a string
-		cout << "Time Server: Recieved: " << bytesRecv << " bytes of \"" << &sockets[index].buffer[len] << "\" message.\n";
+		cout << "[Server] Recieved: " << bytesRecv << " bytes of \"" << &sockets[index].buffer[len] << "\" message.\n";
 
 		sockets[index].len += bytesRecv;
 		string req;
@@ -408,18 +363,18 @@ void receiveMessage(int index)
 void sendMessage(int index)
 {
 	int bytesSent = 0;
-	char sendBuff[255];
+	char sendBuff[1024];
 	SOCKET msgSocket = sockets[index].id;
 
 	strcpy(sendBuff, sockets[index].buffer);
 	bytesSent = send(msgSocket, sendBuff, (int)strlen(sendBuff), 0);
 	if (SOCKET_ERROR == bytesSent)
 	{
-		cout << "Time Server: Error at send(): " << WSAGetLastError() << endl;
+		cout << "[Server] Error at send(): " << WSAGetLastError() << endl;
 		return;
 	}
 
-	cout << "Time Server: Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
+	cout << "[Server] Sent: " << bytesSent << "\\" << strlen(sendBuff) << " bytes of \"" << sendBuff << "\" message.\n";
 	sockets[index].send = IDLE;
 }
 
@@ -643,7 +598,7 @@ string headerToSend()
 	response.append("Date: ");
 	time(&currentTime);
 	response.append(ctime(&currentTime));
-	response += "Server: Omer And Yarin Server\r\nContent-Type: text/html\r\nContent-length: ";
+	response += "[Server] Omer And Yarin Server\r\nContent-Type: text/html\r\nContent-length: ";
 	return response;
 }
 
@@ -708,7 +663,6 @@ void findMethodOfHttpRequest(string &recvReq, int &subType)
 		subType = ERROR_REQUEST;
 	}
 }
-
 
 
 #endif
